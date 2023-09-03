158066cd1e93311444dea509e929a348d0957716
山重水复 <120782591+Shanshui2022@users.noreply.github.com>
2023-09-03 08:40:59 +0800

    加入功能并修改菜单项

---
+++ Ping.py
@@ -0,0 +1,5 @@
+async def ping_main(内容):
+    if 内容 := 内容:
+        return 0
+    else:
+        return "您好像并没有输入IP/域名，请检查命令使用格式"
--- __init__.py
+++ __init__.py
@@ -2,21 +2,27 @@ import nonebot
 from nonebot import on_command #引入on_command参数（必须）
 from nonebot.adapters import Message #引入消息数组
 from nonebot.params import CommandArg #引入消息分词
+from .Ping import *
 配置 = nonebot.get_driver().config
 command_starts = list(nonebot.get_driver().config.command_start)
 default_start = command_starts[0]
 
-
-
-systool = on_command("systool")
-@systool.handle()
+syshelp = on_command("syshelp")
+@syshelp.handle()
 async def handle_function(args: Message = CommandArg()):
     内容 = args.extract_plain_text()  #取命令后跟的内容
     if 内容 := 内容:
-        if 内容 == "help":
-            return 0
+        return 0
     else:
-        await systool.send("您好像没有添加任何参数，已默认输出帮助菜单")
-        await systool.finish(f'''Systools帮助菜单
-{default_start}systool  #Systools菜单
-{default_start}systool ''')
+        await syshelp.finish(f"""Systools帮助菜单
+{command_starts}syshelp | 显示帮助菜单
+{command_starts}ping | Ping网站延迟
+{command_starts}whois | 获取网站Whois信息
+{command_starts}sysinfo | 获取系统信息""")
+        
+ping = on_command("ping")
+@ping.handle()
+async def handle_function(args: Message = CommandArg()):
+    内容 = args.extract_plain_text
+    结果 = await ping_main(内容)
+    await ping.finish(结果)
